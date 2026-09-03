fa7ddce9990f0cdd2beb9bc71b22ce45e2c73dd3
Larissa Ferreira <larissafj@gmail.com>
2026-09-03 17:49:12 -0300

    first commit

---
+++ src/main/java/br/com/raizesdonordeste/backend/controller/SmokeTestController.java
@@ -0,0 +1,18 @@
+package br.com.raizesdonordeste.backend.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SmokeTestController {
+
+    private static final Logger log = LoggerFactory.getLogger(SmokeTestController.class);
+
+    @GetMapping("/api/smoke")
+    public String smokeTest() {
+        log.info("Endpoint de smoke test foi acionado");
+        return "API Raízes do Nordeste funcionando!";
+    }
+}
