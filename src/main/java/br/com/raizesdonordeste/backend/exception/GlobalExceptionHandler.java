5955f73397b752a0052823b0098ee143070f2bc7
Larissa Ferreira <larissafj@gmail.com>
2026-09-14 18:19:42 -0300

    feat: implementa GlobalExceptionHandler

---
+++ src/main/java/br/com/raizesdonordeste/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package br.com.raizesdonordeste.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({
+            ClienteNaoEncontradoException.class,
+            UnidadeNaoEncontradaException.class,
+            ProdutoNaoEncontradoException.class,
+            EstoqueNaoEncontradoException.class,
+            PedidoNaoEncontradoException.class
+    })
+    public ResponseEntity<String> tratarNaoEncontrado(RuntimeException exception) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+}
